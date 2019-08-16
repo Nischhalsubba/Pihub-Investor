91863d848526741c6318506594e508fd89c394e2
rohan <rohan.maleku@choobs.com>
2019-08-16 22:49:50 +0200

    #27 - No Auth routes defined.

---
+++ src/components/_auth/RequireNoAuth.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+export default ChildComponent => {
+    class ComposedComponent extends Component {
+        componentDidMount() {
+            this.shouldNavigateAway();
+        }
+
+        componentDidUpdate() {
+            this.shouldNavigateAway();
+        }
+
+        shouldNavigateAway() {
+            // give false condition
+        }
+
+        render() {
+            return <ChildComponent {...this.props} />;
+        }
+    }
+
+    function mapStateToProps(state) {
+        return {
+            auth: state.auth
+        };
+    }
+
+    return connect(mapStateToProps)(ComposedComponent);
+};
