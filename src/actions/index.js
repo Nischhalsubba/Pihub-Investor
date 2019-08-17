9c75750b21d11f12cb5cfcdb66c585d45e6f2f37
rohan <rohan.maleku@choobs.com>
2019-08-17 15:11:36 +0200

    #53 - http client: axios updated to add bearer token to Authorization Header if token exists and is valid.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,13 @@ import jwt from 'jsonwebtoken';
 let client;
 axios.interceptors.request.use(
     async config => {
-        //if token in localstorage add to all axios call
-
+        //if token in localstorage and has not expired add to all axios call
+        if (localStorage.getItem('token')) {
+            const {exp} = jwt.decode(localStorage.getItem('token'));
+            if ((exp * 1000) > Date.now()) {
+                config.headers.Authorization = `Bearer ` + localStorage.getItem('token');
+            }
+        }
         config.headers.From = process.env.REACT_APP_API_HEADER_FROM;
         return config;
     },
