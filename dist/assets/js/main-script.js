49e8fcbaf7d5d5419f154297553964944d635425
Nischhal Raj Subba <hinischalsubba@gmail.com>
2019-09-02 22:28:12 +0545

    removed the jquery and all the plugin realated to it & fixes on investor layout #91

--- dist/assets/js/main-script.js
+++ dist/assets/js/main-script.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){if($("#file_dropzone").length){var e=document.querySelector("#tpl").innerHTML;$("div#file_dropzone").dropzone({url:"http://google.com",previewTemplate:e,previewsContainer:".file-upload-display",acceptedFiles:"accept"})}
-//hamburger action
-$("#hamburger").click(function(){window.innerWidth;$(this).toggleClass("open"),$(".sidebar").toggleClass("active")})});
+document.addEventListener("DOMContentLoaded",function(){
+// Handler when the DOM is fully loaded
+});
