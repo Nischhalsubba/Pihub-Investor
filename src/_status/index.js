0555ebb04b5cddb2ca427d04f2a1c74d3cd09122
rohan <rohan.maleku@choobs.com>
2019-10-01 10:44:00 +0545

    conditional linking to applicaiton or creditor detail page: @todo decide what should be shown if not yet invested but offer_sent?

--- src/_status/index.js
+++ src/_status/index.js
@@ -54,6 +54,10 @@ export const matchesInvestorStatus = {
         translation_key: 'statuses.offer.accepted_by_investor',
         class: 'badge-warning'
     }, // offer sent by investor
+    invested: {
+        translation_key: 'statuses.offer.accepted_by_investor',
+        class: 'badge-warning'
+    }, // offer sent by investor
     accepted_by_investor: {
         translation_key: 'statuses.offer.accepted_by_investor',
         class: 'badge-warning'
