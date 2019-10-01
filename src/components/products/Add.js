ad9a310a2649afe60c1eee04ace04a935a91e7f1
Anuzshakya <anuz007@hotmail.com>
2019-10-01 10:51:51 +0545

    minor changes

--- src/components/products/Add.js
+++ src/components/products/Add.js
@@ -65,11 +65,9 @@ class AddProduct extends Component {
       
       
       this.setState({ states: this.props.allStates.list, statesWithId: this.props.allStates.all })
-      console.log(this.props.allStates.list);
     }
     if (this.props.states !== prevProps.states) {
       this.props.getCounties(extractIdForName(this.state.states, this.state.statesWithId))
-      console.log(this.state.states);
       
     };
     if (this.props.service !== prevProps.service) {
