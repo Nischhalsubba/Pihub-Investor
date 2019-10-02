b4ff9c9321cc5069279d7475139cfd34b0eb1668
Sanjeev Shrestha <sethstha@gmail.com>
2019-10-02 12:17:33 +0545

    Fix - view product

--- src/components/products/View.js
+++ src/components/products/View.js
@@ -42,7 +42,7 @@ class ViewProduct extends Component {
             return ratings.map((rating, index) => {
                 console.log(rating)
                 return (
-                    <div className="col-3 p-0">
+                    <div className="product-info col-6 col-md-3">
                         <h6>{rating.name}</h6>
                         <span>{rating.value}</span>
                     </div>
@@ -118,92 +118,86 @@ class ViewProduct extends Component {
                         </div> : null}
 
                     </div>
-                    <div className="content-body credit-request">
-                        <div className="d-flex">
-                            <div className="col-lg-12 col-xl-8">
-                                <div className="row justify-content-between w-100">
-                                    <div className="col-2 p-0">
-                                        {/* <h6>Product Title</h6> */}
-                                        <Translate content='label.producttitle' component="h6"/>
-                                        <a href="#">{product_title}</a>
-                                    </div>
-                                    <div className="col-2 p-0">
-                                        <Translate content='label.service' component="h6"/>
-                                        <a>{service ? service.name[Translator.getLocale()] : null} </a>
-                                    </div>
-                                    <div className="col-2 p-0">
-                                        <Translate content='label.state' component="h6"/>
-                                        <a>{states ? this.listStates(states) : null}</a>
-                                    </div>
-                                    <div className="col-2 p-0">
-                                        {/* <Translate content='label.state' component="h6" /> */}
-                                        <Translate content='label.county' component="h6"/>
-                                        <a>{states ? this.listStates(County) : null}</a>
-                                    </div>
-                                    <div className="col-2 p-0">
-                                        <Translate content='label.industries' component="h6"/>
-                                        <div className="d-flex flex-wrap justify-content-between flex-column">
-                                            {industries ? this.listIndustries(industries) : null}
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                                <div className="row justify-content-between w-100 mt-5">
-                                    <div className="col-2 p-0">
-                                        <Translate content='label.minimumsales' component="h6"/>
-                                        <ToEuro amount={min_sales_creditor}/>
-                                    </div>
-                                    <div className="col-2 p-0">
-                                        <Translate content='label.Sicherheiten' component="h6"/>
-                                        <a>{collatoral ? <Translate content='label.yes'/> :
-                                            <Translate content='label.no'/>} </a>
-                                    </div>
-
-                                </div>
-
-
-                                <div className="row justify-content-between w-100 mt-5">
-                                    {ratings ? this.listRating(ratings) : null}
-                                </div>
+                    <div className="content-body product-details">
+                        <div className="row product-info-row">
+                            <div className="product-info col-6 col-md-3">
+                                {/* <h6>Product Title</h6> */}
+                                <Translate content='label.producttitle' component="h6"/>
+                                <a href="#">{product_title}</a>
                             </div>
-                            <div className="col-lg-12 col-xl-4 rightbar">
-                                <div className="amount">
-                                    {/* <h6>Max Credit Amount</h6> */}
-                                    <Translate content='label.maxcredit' component="h6"/>
-                                    <h2><ToEuro amount={max_credit_amount}/></h2>
-                                </div>
-                                <div className="amount mt-5">
-                                    {/* <h6>Max Credit Amount</h6> */}
-                                    {/* <h6>Mindestkreditbetrag</h6> */}
-                                    <Translate content='column.minimum_credit_amount' component="h6"/>
-
-                                    <h2><ToEuro amount={min_credit_amount}/></h2>
-                                </div>
-
-                                <div className="date mt-5">
-                                    {/* <h6>Time Duration</h6> */}
-                                    <Translate content='column.minduration' component="h6"/>
-                                    <a href="#">{min_time_duration} <Translate content='label.months'/> </a>
-                                </div>
-                                <div className="date mt-5">
-                                    {/* <h6>Time Duration</h6> */}
-                                    <Translate content='column.maxduration' component="h6"/>
-                                    <a href="#">{max_time_duration} <Translate content='label.months'/> </a>
-                                </div>
+                            <div className="product-info col-6 col-md-3">
+                                <Translate content='label.service' component="h6"/>
+                                <a>{service ? service.name[Translator.getLocale()] : null} </a>
+                            </div>
+                            <div className="product-info col-6 col-md-3">
+                                <Translate content='label.state' component="h6"/>
+                                <a>{states ? this.listStates(states) : null}</a>
+                            </div>
+                            <div className="product-info col-12 col-md-3 text-left text-md-right">
+                                {/* <h6>Max Credit Amount</h6> */}
+                                <Translate content='label.maxcredit' component="h6"/>
+                                <h2><ToEuro amount={max_credit_amount}/></h2>
                             </div>
                         </div>
-                        <div className="attachments">
+
+                        <div className="row product-info-row">
+                            <div className="product-info col-6 col-md-3">
+                                {/* <Translate content='label.state' component="h6" /> */}
+                                <Translate content='label.county' component="h6"/>
+                                <a>{states ? this.listStates(County) : null}</a>
+                            </div>
+                            <div className="product-info col-6 col-md-3">
+                                <Translate content='label.industries' component="h6"/>
+                                <div className="d-flex flex-wrap justify-content-between flex-column">
+                                    {industries ? this.listIndustries(industries) : null}
+                                </div>
+                            </div>
+                            <div className="product-info col-6 col-md-3">
+                                <Translate content='label.minimumsales' component="h6"/>
+                                <ToEuro amount={min_sales_creditor}/>
+                            </div>
+                            <div className="product-info col-12 col-md-3 text-left text-md-right">
+                                {/* <h6>Max Credit Amount</h6> */}
+                                {/* <h6>Mindestkreditbetrag</h6> */}
+                                <Translate content='column.minimum_credit_amount' component="h6"/>
+
+                                <h2><ToEuro amount={min_credit_amount}/></h2>
+                            </div>
+                        </div>
+
+                        <div className="row product-info-row">
+                            <div className="product-info col-6 col-md-6">
+                                <Translate content='label.Sicherheiten' component="h6"/>
+                                <a>{collatoral ? <Translate content='label.yes'/> :
+                                    <Translate content='label.no'/>} </a>
+                            </div>
+                            <div className="product-info col-6 col-md-6 text-left text-md-right">
+                                {/* <h6>Time Duration</h6> */}
+                                <Translate content='column.minduration' component="h6"/>
+                                <a href="#">{min_time_duration} <Translate content='label.months'/> </a>
+                            </div>
+                        </div>
+
+                        <div className="product-info text-left text-md-right">
+                            {/* <h6>Time Duration</h6> */}
+                            <Translate content='column.maxduration' component="h6"/>
+                            <a href="#">{max_time_duration} <Translate content='label.months'/> </a>
+                        </div>
+
+                        <div className="product-info-row row">
+                            {ratings ? this.listRating(ratings) : null}
+                        </div>
+                        <div className="product-info-files product-info">
                             {/* <h4>Attachments</h4> */}
                             <Translate content='label.attachments' component="h6"/>
                             {/* <div className="file mb-2">
-                <span className="file-name">tax payer investment.docx</span>
-                <span className="ml-4 file-size">400.5kb</span>
-              </div>
-              <div className="file">
-                <span className="file-name">investment agreement.pdf</span>
-                <span className="ml-4 file-size">322.2kb</span>
-              </div> */}
+                            <span className="file-name">tax payer investment.docx</span>
+                            <span className="ml-4 file-size">400.5kb</span>
+                            </div>
+                            <div className="file">
+                            <span className="file-name">investment agreement.pdf</span>
+                            <span className="ml-4 file-size">322.2kb</span>
+                            </div> */}
                             {documents ? this.showAttachments(documents) : null}
                         </div>
                         {status !== 'deleted' ?
