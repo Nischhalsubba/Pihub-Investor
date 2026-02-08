c5afe5026024dee98610f125a0e6ec49d48b2f49
Nischhal <hinischalsubba@gmail.com>
2026-02-08 13:22:06 +0545

    Updates

---
+++ precache-manifest.10564e022e14e212d8c3a1caae7bce33.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4e140cdc776c86e749ecbe58662e54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77846336560f5255dc22",
+    "url": "./static/css/2.0c7200f9.chunk.css"
+  },
+  {
+    "revision": "77846336560f5255dc22",
+    "url": "./static/js/2.40c70317.chunk.js"
+  },
+  {
+    "revision": "b0ad197ffb28318f1714",
+    "url": "./static/js/main.b4e7006b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "./static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "./static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "./static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "./static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "./static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
