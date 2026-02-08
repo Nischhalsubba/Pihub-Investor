1c62eaa9e58cac04808bde869964b478eaf0320b
Nischhal <hinischalsubba@gmail.com>
2026-02-08 23:22:02 +0545

    Add rw-widgets font files and SVG icons
    - Added SVG file for rw-widgets containing various glyphs including up-dir, search, down-dir, calendar, clock, angle-left, and angle-right.
    - Introduced EOT and TTF font files for rw-widgets to support web font usage.
    - Ensured proper licensing metadata is included in the font files.

---
+++ build/precache-manifest.2239931e0fd17e6c657ca6db6c3fa2e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af4735cb09e34357c91a2a5b9dd861f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b45dfb21b356ba99f5b",
+    "url": "./static/css/2.0c7200f9.chunk.css"
+  },
+  {
+    "revision": "4b45dfb21b356ba99f5b",
+    "url": "./static/js/2.6d24df17.chunk.js"
+  },
+  {
+    "revision": "6a9add393af1e4885f40",
+    "url": "./static/js/main.dc264a6f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "./static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "./static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "./static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "./static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "./static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
