6afeaf2f8b28f6077b000e14754c2c32fce642cf
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-21 21:28:25 +0545

    feat(ui): add editorial page hierarchy

--- app/src/components/general/Subheader.js
+++ app/src/components/general/Subheader.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-export default props => {
-  return (
-    <div className="content-head">
-      <div className="content-head-left">
-        <h1 className="content-head__title">{props.heading}</h1>
-      </div>
-      {props.buttonLabel ? (
-        <div className="content-head-right">
-          <Link className="btn btn-primary" to={props.link}>
-            {props.buttonLabel}
-          </Link>
-        </div>
-      ) : null}
+
+export default props => (
+  <div className="content-head" data-motion="page-head">
+    <div className="content-head-left">
+      <div className="content-head-kicker">Opportunity intelligence</div>
+      <h1 className="content-head__title">{props.heading}</h1>
+      <p className="content-head-copy">
+        Review credit opportunities with a clearer view of range, duration, sector exposure and current status.
+      </p>
     </div>
-  );
-};
+    {props.buttonLabel ? (
+      <div className="content-head-right">
+        <Link className="btn btn-primary" to={props.link}>{props.buttonLabel}</Link>
+      </div>
+    ) : null}
+  </div>
+);
