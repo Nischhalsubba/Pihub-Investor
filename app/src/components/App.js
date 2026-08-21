4e3cbc34a690919c3fbe457d42b5e2206be154c6
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-21 21:39:35 +0545

    feat(ui): make application shell route-aware and accessible

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import Sidebar from './general/Sidebar';
 import Header from './general/Header';
-// import Pagination from './general/Pagination';
+
 class App extends Component {
+  mainRef = React.createRef();
+
+  componentDidMount() {
+    this.signalRouteReady();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.pathname !== this.props.location.pathname) {
+      if (this.mainRef.current) {
+        try {
+          this.mainRef.current.focus({ preventScroll: true });
+        } catch (error) {
+          this.mainRef.current.focus();
+        }
+      }
+      this.signalRouteReady();
+    }
+  }
+
+  signalRouteReady = () => {
+    window.requestAnimationFrame(() => {
+      window.dispatchEvent(new CustomEvent('pihub:route-ready'));
+    });
+  };
+
   render() {
     return (
-      <div className="container-full-height ct-container">
+      <div className="workspace-shell ct-container">
+        <a className="skip-link" href="#main-content">Skip to main content</a>
         <Sidebar />
         <div className="main-content main-content--padded">
           <Header />
-          {this.props.children}
-          {/* <Pagination /> */}
+          <main id="main-content" className="workspace-main" ref={this.mainRef} tabIndex="-1">
+            {this.props.children}
+          </main>
         </div>
       </div>
     );
   }
 }
-export default App;
+
+export default withRouter(App);
