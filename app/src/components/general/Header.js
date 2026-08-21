36799e8abe1825c159b8d42486a2017b031b6c10
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-21 18:29:50 +0545

    feat(ui): modernize accessible account header

--- app/src/components/general/Header.js
+++ app/src/components/general/Header.js
@@ -14,15 +14,18 @@ counterpart.registerTranslations('de', de);
 counterpart.setLocale(
   localStorage.getItem('language') || navigator.language.split('-')[0] || 'de'
 );
-class Header extends Component {
-  state = { language: localStorage.getItem('language') || navigator.language.split('-')[0] || 'de' }
-  onChange = e => {
 
-    // let language = e.target.value;
-    counterpart.setLocale(e);
-    this.setState({ language: e })
-    this.props.changeLanguage(e);
-  }
+class Header extends Component {
+  state = {
+    language: localStorage.getItem('language') || navigator.language.split('-')[0] || 'de'
+  };
+
+  onChange = language => {
+    counterpart.setLocale(language);
+    this.setState({ language });
+    this.props.changeLanguage(language);
+  };
+
   componentDidMount() {
     this.props.getNotificationCount();
   }
@@ -30,67 +33,55 @@ class Header extends Component {
   render() {
     return (
       <header className="site-header">
-        <div className="logo-container">
-          <img src="/assets/img/logo.png" alt="Pinhub Logo" />
-        </div>
-        <nav className="header-actions">
+        <Link className="logo-container" to="/products" aria-label="Pihub Investor home">
+          <img src="/assets/img/logo.png" alt="Pihub" />
+        </Link>
+        <nav className="header-actions" aria-label="Account actions">
           <ul>
-            <ul className="language-changer">
-              <li>
-                <button onClick={() => this.onChange('en')}>
-                  <img src="assets/img/gb.svg" alt="English Language" />
-                  English
+            <li>
+              <ul className="language-changer" aria-label="Language">
+                <li>
+                  <button type="button" onClick={() => this.onChange('en')} aria-pressed={this.state.language === 'en'}>
+                    <img src="/assets/img/gb.svg" alt="" />English
                   </button>
-              </li>
-              <li>
-                <button onClick={() => this.onChange('de')}>
-                  <img src="assets/img/de.svg" alt="Deutsch Language" />
-                  Deutsch
+                </li>
+                <li>
+                  <button type="button" onClick={() => this.onChange('de')} aria-pressed={this.state.language === 'de'}>
+                    <img src="/assets/img/de.svg" alt="" />Deutsch
                   </button>
-              </li>
-            </ul>
-
+                </li>
+              </ul>
+            </li>
             <li className="header-actions__item">
-
-              <Link className="header-notification" to="/notifications">
-                <i className="bx bx-bell" />
-                <span className="notification-count">{this.props.count}</span>
+              <Link className="header-notification" to="/notifications" aria-label={`${this.props.count || 0} notifications`}>
+                <i className="bx bx-bell" aria-hidden="true" />
+                {this.props.count ? <span className="notification-count">{this.props.count}</span> : null}
               </Link>
             </li>
-
-
-            <div className="dropdown">
-              <a className=" dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img src="/assets/img/user.png" alt="John Doe" />
-              </a>
+            <li className="dropdown">
+              <button className="dropdown-toggle" id="dropdownMenuButton" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="Open account menu">
+                <img src="/assets/img/user.png" alt="" />
+              </button>
               <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                <Link className="dropdown-item" to='/user/profile'>
-                  <Translate content='label.profile' />
-                </Link>
-                <Link className="dropdown-item" to='/user/edit-profile'>
-                  <Translate content='label.editprofile' />
-
-                </Link>
-                <Link className="dropdown-item" to='/change-password'>
-                  <Translate content='label.resetpassword' />
-                </Link>
-                <span className="dropdown-item" onClick={() => {
-                  this.props.logout(() => this.props.history.push('/login'))
-                }}>
-                  <Translate content='label.logout' />
-
-                </span>
+                <Link className="dropdown-item" to="/user/profile"><Translate content="label.profile" /></Link>
+                <Link className="dropdown-item" to="/user/edit-profile"><Translate content="label.editprofile" /></Link>
+                <Link className="dropdown-item" to="/change-password"><Translate content="label.resetpassword" /></Link>
+                <button className="dropdown-item" type="button" onClick={() => this.props.logout(() => this.props.history.push('/login'))}>
+                  <Translate content="label.logout" />
+                </button>
               </div>
-            </div>
+            </li>
           </ul>
         </nav>
       </header>
     );
   }
 }
+
 function mapStateToProps(state) {
   return { count: state.notificationCount, language: state.language };
 }
+
 export default connect(
   mapStateToProps,
   { getNotificationCount, logout, changeLanguage }
