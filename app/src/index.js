c12f474c79ed8dcc3e21ad1838e016bd0517f017
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-22 09:56:59 +0545

    fix(ui): surface authenticated render failures

--- app/src/index.js
+++ app/src/index.js
@@ -10,6 +10,7 @@ import de from './_locale/de';
 import { getStoredToken } from './_utils/authToken';
 
 import App from './components/App';
+import AppErrorBoundary from './components/general/AppErrorBoundary';
 import reducers from './reducers';
 import NoMatch from './components/general/NoMatch';
 import TermsCondition from './components/general/TermsCondition';
@@ -70,104 +71,106 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        {/** ___ Start: NO AUTH ROUTES --- Authenticated users should be redirected to home screen ---*/}
-        <Route path="/login" exact component={RequireNoAuth(Login)} />
-        <Route
-          path="/set-password/:token"
-          exact
-          component={RequireNoAuth(SetPassword)}
-        />
-        <Route
-          path="/forgot-password"
-          exact
-          component={RequireNoAuth(ForgotPassword)}
-        />
-        <Route path="/logout" exact component={RequireInvestorAuth(Signout)} />
-        <Route path="/signup" exact component={RequireNoAuth(Signup)} />
-        <Route path='/change-password' component={RequireInvestorAuth(ChangePassword)} />
-        {/** --- End: NO AUTH ROUTES --- Authenticated users should be redirected to home screen ___ */}
-        {/** ___ Start: signup protected routes (auth for these routes handled within component itself) --- */}
-        <Route path="/signup/activated" exact component={SignUpActivated} />
-        {/** Logged in and activated accounts investors only*/}
-        <Route
-          path="/signup/confirm-email"
-          exact
-          component={ConfirmEmail}
-        />{' '}
-        {/** Logged in (there should be a token) and unconfirmed accounts only*/}
-        <Route path="/signup/confirmation" exact component={Confirmation} />{' '}
-        {/** Logged in (there should be a token) and unconfirmed accounts only*/}
-        <Route path="/confirm/:hash" exact component={Approval} />{' '}
-        {/** Logged in (there should be a token) - confirmed but waiting to e approved accounts only*/}
-        <Route path='/password-change-success' component={PasswordChangeSuccess} />
-        {/** --- End: signup protected routes ___ */}
-        <Route path='/terms-and-conditions' component={TermsCondition} />
+    <AppErrorBoundary>
+      <BrowserRouter>
+        <Switch>
+          {/** ___ Start: NO AUTH ROUTES --- Authenticated users should be redirected to home screen ---*/}
+          <Route path="/login" exact component={RequireNoAuth(Login)} />
+          <Route
+            path="/set-password/:token"
+            exact
+            component={RequireNoAuth(SetPassword)}
+          />
+          <Route
+            path="/forgot-password"
+            exact
+            component={RequireNoAuth(ForgotPassword)}
+          />
+          <Route path="/logout" exact component={RequireInvestorAuth(Signout)} />
+          <Route path="/signup" exact component={RequireNoAuth(Signup)} />
+          <Route path='/change-password' component={RequireInvestorAuth(ChangePassword)} />
+          {/** --- End: NO AUTH ROUTES --- Authenticated users should be redirected to home screen ___ */}
+          {/** ___ Start: signup protected routes (auth for these routes handled within component itself) --- */}
+          <Route path="/signup/activated" exact component={SignUpActivated} />
+          {/** Logged in and activated accounts investors only*/}
+          <Route
+            path="/signup/confirm-email"
+            exact
+            component={ConfirmEmail}
+          />{' '}
+          {/** Logged in (there should be a token) and unconfirmed accounts only*/}
+          <Route path="/signup/confirmation" exact component={Confirmation} />{' '}
+          {/** Logged in (there should be a token) and unconfirmed accounts only*/}
+          <Route path="/confirm/:hash" exact component={Approval} />{' '}
+          {/** Logged in (there should be a token) - confirmed but waiting to e approved accounts only*/}
+          <Route path='/password-change-success' component={PasswordChangeSuccess} />
+          {/** --- End: signup protected routes ___ */}
+          <Route path='/terms-and-conditions' component={TermsCondition} />
 
-        <App>
-          {/** ___ Start: Authenticated User's routes --- */}
-          <Route path="/" exact component={RequireInvestorAuth(RequireVerification(ProductsList))} />
-          <Route
-            exact
-            path="/products"
-            component={RequireInvestorAuth(RequireVerification(ProductsList))}
-          />
-          <Route
-            path="/add-product"
-            component={RequireInvestorAuth(RequireVerification(AddProduct))}
-          />
-          <Route
-            exact
-            path="/edit-product"
-            component={RequireInvestorAuth(RequireVerification(EditProduct))}
-          />
-          <Route
-            exact
-            path="/products-invested"
-            component={RequireInvestorAuth(RequireVerification(InvestedList))}
-          />
-          <Route
-            exact
-            path="/products-applications"
-            component={RequireInvestorAuth(AppliedList)}
-          />
-          <Route
-            exact
-            path="/product"
-            component={RequireInvestorAuth(ViewProduct)}
-          />
-          <Route
-            exact
-            path="/product/applications"
-            component={RequireInvestorAuth(ApplicationList)}
-          />
-          <Route
-            path="/application"
-            component={RequireInvestorAuth(DetailCreditRequest)}
-          />
-          <Route
-            exact
-            path="/notifications"
-            component={RequireInvestorAuth(Notifications)}
-          />
-          {/* Credit requests related route definition */}
-          <Route path="/creditor/detail" exact component={RequireInvestorAuth(CreditorDetail)} />
-          <Route
-            exact
-            path="/credit-request"
-            component={RequireInvestorAuth(RequireVerification(ListCreditRequest))}
-          />
-          {/* Profile */}
-          <Route path='/user/profile' component={RequireInvestorAuth(ViewProfile)} />
-          <Route path='/user/edit-profile' component={RequireInvestorAuth(EditProfile)} />
-          <Route path='/account-unverified' component={RequireInvestorAuth(UnverifiedPage)} />
-          {/* <Redirect from="/credit-request/detail" to="/product" /> */}
-          {/** --- End: Authenticated User's routes ___ */}
-        </App>
-        {/* <Route component={NoMatch} /> */}
-      </Switch>
-    </BrowserRouter>
+          <App>
+            {/** ___ Start: Authenticated User's routes --- */}
+            <Route path="/" exact component={RequireInvestorAuth(RequireVerification(ProductsList))} />
+            <Route
+              exact
+              path="/products"
+              component={RequireInvestorAuth(RequireVerification(ProductsList))}
+            />
+            <Route
+              path="/add-product"
+              component={RequireInvestorAuth(RequireVerification(AddProduct))}
+            />
+            <Route
+              exact
+              path="/edit-product"
+              component={RequireInvestorAuth(RequireVerification(EditProduct))}
+            />
+            <Route
+              exact
+              path="/products-invested"
+              component={RequireInvestorAuth(RequireVerification(InvestedList))}
+            />
+            <Route
+              exact
+              path="/products-applications"
+              component={RequireInvestorAuth(AppliedList)}
+            />
+            <Route
+              exact
+              path="/product"
+              component={RequireInvestorAuth(ViewProduct)}
+            />
+            <Route
+              exact
+              path="/product/applications"
+              component={RequireInvestorAuth(ApplicationList)}
+            />
+            <Route
+              path="/application"
+              component={RequireInvestorAuth(DetailCreditRequest)}
+            />
+            <Route
+              exact
+              path="/notifications"
+              component={RequireInvestorAuth(Notifications)}
+            />
+            {/* Credit requests related route definition */}
+            <Route path="/creditor/detail" exact component={RequireInvestorAuth(CreditorDetail)} />
+            <Route
+              exact
+              path="/credit-request"
+              component={RequireInvestorAuth(RequireVerification(ListCreditRequest))}
+            />
+            {/* Profile */}
+            <Route path='/user/profile' component={RequireInvestorAuth(ViewProfile)} />
+            <Route path='/user/edit-profile' component={RequireInvestorAuth(EditProfile)} />
+            <Route path='/account-unverified' component={RequireInvestorAuth(UnverifiedPage)} />
+            {/* <Redirect from="/credit-request/detail" to="/product" /> */}
+            {/** --- End: Authenticated User's routes ___ */}
+          </App>
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
+      </BrowserRouter>
+    </AppErrorBoundary>
   </Provider>,
   document.querySelector('#root')
 );
