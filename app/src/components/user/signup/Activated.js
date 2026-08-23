fbf321ae82a0085a0a1f8cad31dea27f86b6919e
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-23 12:28:37 +0545

    fix(signup): use ESM translator on activation

--- app/src/components/user/signup/Activated.js
+++ app/src/components/user/signup/Activated.js
@@ -4,7 +4,7 @@ import Translate from 'react-translate-component';
 import AuthShell from '../AuthShell';
 import SignupProgress from './SignupProgress';
 
-const Translator = require('react-translate-component');
+const Translator = Translate;
 
 class SignUpActivated extends Component {
   componentDidMount() {
