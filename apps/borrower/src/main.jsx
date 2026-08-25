eb1bae0605f647793a71c2eb909479221d650bf2
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-25 15:29:40 +0545

    Apply Investor workspace design to Borrower

--- apps/borrower/src/main.jsx
+++ apps/borrower/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom-v6';
 import '../../../packages/ui/src/platform.css';
 import '../../../packages/ui/src/containment.css';
+import '../../../packages/ui/src/investor-workspace.css';
 import './styles.css';
 import App from './App';
 createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
