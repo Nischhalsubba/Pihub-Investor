d3513d289e803eec08bb8c27b3d7b43f5b6c5a61
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-25 12:52:38 +0545

    Contain mobile platform tables within app shell

--- apps/access/src/main.jsx
+++ apps/access/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import '../../../packages/ui/src/platform.css';
+import '../../../packages/ui/src/containment.css';
 import './styles.css';
 import App from './App';
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
